51bbebb2194f0254f6660f168841bb0b34c1a87c
sktoushi <111473280+sktoushi@users.noreply.github.com>
2024-08-19 18:19:30 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 // config.js
 const UPDATE_VISITOR_API = "https://script.google.com/macros/s/AKfycbx-c7HUH9OnQBMwDAss1WpMV0uZAs8zHwGEHSn9EIQ_hTRBKVYLiPStXQTkbhfpcS7T/exec"; // 20240819_1530
 const PAYOFFS_API = "https://script.google.com/macros/s/AKfycbwrWbrBcNGcqI-4wzXS3SlzKt7Dqscv9vQrC3Gv_9ZAxFLckIIuh4bpVrMY79XlDPaL/exec"; // 240819_1739
+const SHEET_URL = "https://docs.google.com/spreadsheets/d/1Xr-9rsQxbpw3oCNziKu0Lr9dwKpvJaZWFGRE9ECa-Ps/edit?usp=sharing"; // Payoffs Sheet // 240819_1819
