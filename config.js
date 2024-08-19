2642885f1ef2b4bd16c85367f5d49468c2d0a7e4
sktoushi <111473280+sktoushi@users.noreply.github.com>
2024-08-19 17:39:17 +0800

    Update config.js - PAYOFFS_API

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 // config.js
 const UPDATE_VISITOR_API = "https://script.google.com/macros/s/AKfycbx-c7HUH9OnQBMwDAss1WpMV0uZAs8zHwGEHSn9EIQ_hTRBKVYLiPStXQTkbhfpcS7T/exec"; // 20240819_1530
+const PAYOFFS_API = "https://script.google.com/macros/s/AKfycbwrWbrBcNGcqI-4wzXS3SlzKt7Dqscv9vQrC3Gv_9ZAxFLckIIuh4bpVrMY79XlDPaL/exec"; // 240819_1739
