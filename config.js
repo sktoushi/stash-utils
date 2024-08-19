64c88c772d9db4b64b3d81a9d28788ea49072d9a
sktoushi <111473280+sktoushi@users.noreply.github.com>
2024-08-19 15:30:39 +0800

    API URL 最新化

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // config.js
-const UPDATE_VISITOR_API = "https://script.google.com/macros/s/AKfycbzLf1sEPBLVT7sH4yewZOIDf27nhiM-tJuybwa9TYFgiwySps1i898xW20tuD8Vx8c/exec";
+const UPDATE_VISITOR_API = "https://script.google.com/macros/s/AKfycbx-c7HUH9OnQBMwDAss1WpMV0uZAs8zHwGEHSn9EIQ_hTRBKVYLiPStXQTkbhfpcS7T/exec"; // 20240819_1530
